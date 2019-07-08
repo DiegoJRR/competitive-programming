11f325d83a67954a07dfdb77584904c2653f0437
Diego Ramirez <e.silva@hlcn.mx>
2019-07-08 10:54:14 -0500

    Progress in the mental rotation problem

--- Contests/2019 ICPC Malaysia National/A-Mental_Rotation/mental.cpp
+++ Contests/2019 ICPC Malaysia National/A-Mental_Rotation/mental.cpp
@@ -1,8 +1,37 @@
-#include<iostream>
+#include <iostream>
+#include <string>
 
 using namespace std;
 
-int main(){
-	cout<<"Hello world!"<<endl;	
-	return 0;
+int main()
+{
+    int N, rotations=0;
+    string rot;
+    string pos = "^>v<^>v<";
+
+    cin>>N>>rot;
+    
+		char in[N][N];
+
+		for (int i=1; i<=N;i++){
+			for (int j=1; j<=N;j++){
+					cin>>in[i][j];
+        }
+    }
+		cout<<rot<<endl;
+		//For every direction character in the rot string
+		for(int i=0;i<rot.length();i++) {
+			  cout<<rot[i]<<endl;
+				if (rot[i] == 'R'){
+					rotations ++;
+				}
+				else {
+					rotations += 3;
+				}
+		}
+
+		rotations = rotations - (rotations/4)*4;
+    
+		cout<<rotations<<endl;
+    return 0;
 }
